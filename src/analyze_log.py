ca630af70fa8c19dc63a7a25b97f99273d5e086e
bruh-git <brunac.cardososilva@gmail.com>
2022-12-20 20:25:20 -0300

    feat: iniciando a implementação do requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,19 @@
+import csv
+from collections import defaultdict
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file, "r") as file:
+            data = csv.reader(file)
+            for orders in data:
+                default = defaultdict(list)
+                default["cliente"].append(orders[0])
+                default["pedido"].append(orders[1])
+                default["dia"].append(orders[2])
+
+                print(orders)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
