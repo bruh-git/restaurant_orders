c62e168fcf244b98390453c1cbb007a2602641bb
bruh-git <brunac.cardososilva@gmail.com>
2022-12-21 14:25:54 -0300

    feat: req 01

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,19 +1,34 @@
 import csv
+from track_orders import TrackOrders
 from collections import defaultdict
 
 
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    track_orders = TrackOrders()
     try:
         with open(path_to_file, "r") as file:
             data = csv.reader(file)
             for orders in data:
                 default = defaultdict(list)
-                default["cliente"].append(orders[0])
-                default["pedido"].append(orders[1])
-                default["dia"].append(orders[2])
-
-                print(orders)
+                default["custumer"].append(orders[0])
+                default["order"].append(orders[1])
+                default["day"].append(orders[2])
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    maria_ordered_dish = track_orders.get_number_of_orders_per_dish("maria")
+    arnald_burguer = track_orders.get_customer_count_dish(
+        "arnaldo", "hamburguer")
+    joao_never_ordered = track_orders.get_never_ordered_per_customer("joao")
+    joao_never_visited_days = track_orders.get_days_never_visited_per_customer(
+        "joao"
+    )
+    with open("data/mkt_campaign.txt", mode="w", encoding="utf-8") as file:
+        file.write(
+            f"{maria_ordered_dish}\n"
+            f"{arnald_burguer}\n"
+            f"{joao_never_ordered}\n"
+            f"{joao_never_visited_days}\n"
+        )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,44 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self._data = []
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._data.append([customer, order, day])
 
-    def get_most_ordered_dish_per_customer(self, customer):
-        pass
+    def get_number_of_orders_per_dish(self, customer):
+        customer_orders = [
+            order[1] for order in self._data if order[0] == customer
+        ]
+        return Counter(customer_orders).most_common()[0][0]
+
+    def get_customer_count_dish(self, customer, dish):
+        custumer_orders = [
+            order[1]
+            for order in self._data
+            if order[0] == customer and order[1] == dish
+        ]
+        return len(custumer_orders)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        dishes = set(order[1] for order in self._data)
+        customer_orders = set(
+            order[1] for order in self._data if order[0] == customer
+        )
+        return dishes - customer_orders
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days = set(order[2] for order in self._data)
+        customer_days = set(
+            order[2] for order in self._data if order[0] == customer
+        )
+        return days - customer_days
 
     def get_busiest_day(self):
         pass
